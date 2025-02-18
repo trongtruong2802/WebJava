2f8e22c597836ffa8dcb90e8889a3b69d744b02a
Le Thanh Dat <2002lethanhdat2002@gmail.com>
2025-02-18 12:31:06 +0700

    admin

--- src/main/java/com/webecommerce/web/controller/AdminController.java
+++ src/main/java/com/webecommerce/web/controller/AdminController.java
@@ -2,25 +2,31 @@ package com.webecommerce.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
+@RequestMapping("/admin")
+
 public class AdminController {
-    @GetMapping("/admin")
+
+    @A
+    //localhost:5000/admin
+    @GetMapping("/")
     public String admin() {
-        return "/admin/admin";
+        return "admin/index";
     }
+    //localhost:5000/admin/product
     @GetMapping("/product")
     public String product() {
         return "/admin/add_product";
     }
+    //localhost:5000/admin/category
     @GetMapping("/category")
     public String category() {
         return "/admin/category";
     }
-    @GetMapping("/index")
-    public String index() {
-        return "/admin/index";
-    }
+    
 }
